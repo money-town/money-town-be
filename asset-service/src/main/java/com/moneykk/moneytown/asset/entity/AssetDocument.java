944af6499a14cab4e960acdd9e35e6fc566109c8
TaeHeeKim <eksrnsdhkdwk@gmail.com>
2026-08-31 15:30:37 +0900

    fix - ci 실패 수정

--- asset-service/src/main/java/com/moneykk/moneytown/asset/entity/AssetDocument.java
+++ asset-service/src/main/java/com/moneykk/moneytown/asset/entity/AssetDocument.java
@@ -12,6 +12,8 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -49,7 +51,8 @@ public class AssetDocument extends BaseEntity {
     @Column(name = "file_size", nullable = false)
     private long fileSize;
 
-    @Column(name = "file_hash", nullable = false, columnDefinition = "CHAR(64)")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(name = "file_hash", nullable = false, length = 64)
     private String fileHash;
 
     @Column(name = "is_deleted", nullable = false)
