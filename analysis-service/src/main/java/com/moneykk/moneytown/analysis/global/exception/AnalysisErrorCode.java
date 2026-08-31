bdecbd55fc67e20f3bc083602e2cc7c6e9527465
nadaa <nadaa@ajou.ac.kr>
2026-09-01 01:03:42 +0900

    feat: FDS 공통 리포지토리 추가 및 에러코드/차단해제 가드 보완

--- analysis-service/src/main/java/com/moneykk/moneytown/analysis/global/exception/AnalysisErrorCode.java
+++ analysis-service/src/main/java/com/moneykk/moneytown/analysis/global/exception/AnalysisErrorCode.java
@@ -10,7 +10,8 @@ public enum AnalysisErrorCode implements ErrorCode {
     FDS_STATE_NOT_FOUND(HttpStatus.NOT_FOUND, "FDS_404_01", "대상 사용자의 FDS 상태 정보가 존재하지 않습니다."),
     FDS_ALREADY_NORMAL(HttpStatus.CONFLICT, "FDS_409_01", "이미 정상 상태인 사용자입니다."),
     FDS_ALREADY_BLOCKED(HttpStatus.CONFLICT, "FDS_409_02", "이미 차단된 사용자입니다."),
-    FDS_FORBIDDEN(HttpStatus.FORBIDDEN, "FDS_403_01", "FDS 접근 권한이 없습니다.");
+    FDS_FORBIDDEN(HttpStatus.FORBIDDEN, "FDS_403_01", "FDS 접근 권한이 없습니다."),
+    FDS_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "FDS_503_01", "일시적으로 FDS 검사를 수행할 수 없습니다.");
 
 
 
