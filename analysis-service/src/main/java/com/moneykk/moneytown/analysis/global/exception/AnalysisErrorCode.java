20564562ef96c75ccb8b3883f85295709fcee99a
nadaa <nadaa@ajou.ac.kr>
2026-09-01 19:00:27 +0900

    feat: Notification error 코드 추가

--- analysis-service/src/main/java/com/moneykk/moneytown/analysis/global/exception/AnalysisErrorCode.java
+++ analysis-service/src/main/java/com/moneykk/moneytown/analysis/global/exception/AnalysisErrorCode.java
@@ -10,9 +10,13 @@ public enum AnalysisErrorCode implements ErrorCode {
     FDS_STATE_NOT_FOUND(HttpStatus.NOT_FOUND, "FDS_404_01", "대상 사용자의 FDS 상태 정보가 존재하지 않습니다."),
     FDS_ALREADY_NORMAL(HttpStatus.CONFLICT, "FDS_409_01", "이미 정상 상태인 사용자입니다."),
     FDS_ALREADY_BLOCKED(HttpStatus.CONFLICT, "FDS_409_02", "이미 차단된 사용자입니다."),
-    FDS_FORBIDDEN(HttpStatus.FORBIDDEN, "FDS_403_01", "FDS 접근 권한이 없습니다.");
+    FDS_FORBIDDEN(HttpStatus.FORBIDDEN, "FDS_403_01", "FDS 접근 권한이 없습니다."),
 
 
+
+    // ====== Notification ======
+    NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTI_404_01", "해당하는 알림 정보가 존재하지 않습니다."),
+    NOTIFICATION_ALREADY_FINISHED(HttpStatus.CONFLICT, "NOTI_409_01", "이미 처리된 알림입니다.");
 
     private final HttpStatus status;
     private final String code;
