b62dcd7739c1de5e9d090d8d8ea965bee92416f4
nadaa <nadaa@ajou.ac.kr>
2026-09-01 11:33:04 +0900

    chore: api prefix 설정

--- analysis-service/src/main/java/com/moneykk/moneytown/analysis/fds/command/controller/FdsCheckCommandController.java
+++ analysis-service/src/main/java/com/moneykk/moneytown/analysis/fds/command/controller/FdsCheckCommandController.java
@@ -8,10 +8,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class FdsCheckCommandController {
 
     private final PreFdsCheckService preFdsCheckService;
