a30d61ae34413319eb0dfdb2e2844b949cfca1f8
nadaa <nadaa@ajou.ac.kr>
2026-09-03 22:51:29 +0900

    chore: pre/post 기준 prefix 정리

--- analysis-service/src/main/java/com/moneykk/moneytown/analysis/fds/domain/repository/FdsDetectionLogRepository.java
+++ analysis-service/src/main/java/com/moneykk/moneytown/analysis/fds/domain/repository/FdsDetectionLogRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface FdsDetectionLogRepository extends JpaRepository<FdsDetectionLog, UUID> {
+    boolean existsByEventId(UUID eventId);
 }
