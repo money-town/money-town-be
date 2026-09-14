d9368f4aaf4d06cb836ad89d8bb8cd29fb5e4aba
poppq03 <poppq03@naver.com>
2026-09-14 17:44:14 +0900

    feat: Kafka 컨슈머 공통 에러 핸들러 및 DLQ 라우팅 추가

--- wallet-service/src/main/java/com/moneykk/moneytown/wallet/global/config/KafkaConsumerConfig.java
+++ wallet-service/src/main/java/com/moneykk/moneytown/wallet/global/config/KafkaConsumerConfig.java
@@ -12,6 +12,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 // 이벤트는 전부 EventEnvelope<T>로 오므로 payload 타입별 JavaType으로 바인딩한다.
@@ -28,9 +33,10 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, EventEnvelope<Object>> userAccountEventKafkaListenerContainerFactory(
-            ConsumerFactory<String, EventEnvelope<Object>> userAccountEventConsumerFactory
+            ConsumerFactory<String, EventEnvelope<Object>> userAccountEventConsumerFactory,
+            CommonErrorHandler kafkaConsumerErrorHandler
     ) {
-        return containerFactory(userAccountEventConsumerFactory);
+        return containerFactory(userAccountEventConsumerFactory, kafkaConsumerErrorHandler);
     }
 
     @Bean
@@ -42,9 +48,10 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, EventEnvelope<SubscriptionReservedPayload>> subscriptionReservedKafkaListenerContainerFactory(
-            ConsumerFactory<String, EventEnvelope<SubscriptionReservedPayload>> subscriptionReservedConsumerFactory
+            ConsumerFactory<String, EventEnvelope<SubscriptionReservedPayload>> subscriptionReservedConsumerFactory,
+            CommonErrorHandler kafkaConsumerErrorHandler
     ) {
-        return containerFactory(subscriptionReservedConsumerFactory);
+        return containerFactory(subscriptionReservedConsumerFactory, kafkaConsumerErrorHandler);
     }
 
     @Bean
@@ -54,9 +61,10 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, EventEnvelope<Object>> subscriptionConfirmedKafkaListenerContainerFactory(
-            ConsumerFactory<String, EventEnvelope<Object>> subscriptionConfirmedConsumerFactory
+            ConsumerFactory<String, EventEnvelope<Object>> subscriptionConfirmedConsumerFactory,
+            CommonErrorHandler kafkaConsumerErrorHandler
     ) {
-        return containerFactory(subscriptionConfirmedConsumerFactory);
+        return containerFactory(subscriptionConfirmedConsumerFactory, kafkaConsumerErrorHandler);
     }
 
     @Bean
@@ -68,9 +76,23 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, EventEnvelope<SubscriptionCompensationRequestedPayload>> subscriptionCompensationRequestedKafkaListenerContainerFactory(
-            ConsumerFactory<String, EventEnvelope<SubscriptionCompensationRequestedPayload>> subscriptionCompensationRequestedConsumerFactory
+            ConsumerFactory<String, EventEnvelope<SubscriptionCompensationRequestedPayload>> subscriptionCompensationRequestedConsumerFactory,
+            CommonErrorHandler kafkaConsumerErrorHandler
     ) {
-        return containerFactory(subscriptionCompensationRequestedConsumerFactory);
+        return containerFactory(subscriptionCompensationRequestedConsumerFactory, kafkaConsumerErrorHandler);
+    }
+
+    // 1초→2초→4초 간격 3회 재시도, 그래도 실패하면 "{원본토픽}-dlt"로 보내고 다음 메시지로 넘어간다.
+    @Bean
+    public CommonErrorHandler kafkaConsumerErrorHandler(KafkaOperations<Object, Object> kafkaOperations) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaOperations);
+
+        ExponentialBackOffWithMaxRetries backOff = new ExponentialBackOffWithMaxRetries(3);
+        backOff.setInitialInterval(1_000L);
+        backOff.setMultiplier(2.0);
+        backOff.setMaxInterval(10_000L);
+
+        return new DefaultErrorHandler(recoverer, backOff);
     }
 
     private <T> ConsumerFactory<String, EventEnvelope<T>> envelopeConsumerFactory(KafkaProperties kafkaProperties, Class<T> payloadType) {
@@ -86,9 +108,11 @@ public class KafkaConsumerConfig {
         );
     }
 
-    private <T> ConcurrentKafkaListenerContainerFactory<String, T> containerFactory(ConsumerFactory<String, T> consumerFactory) {
+    private <T> ConcurrentKafkaListenerContainerFactory<String, T> containerFactory(ConsumerFactory<String, T> consumerFactory,
+                                                                                     CommonErrorHandler errorHandler) {
         ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler);
 
         return factory;
     }
