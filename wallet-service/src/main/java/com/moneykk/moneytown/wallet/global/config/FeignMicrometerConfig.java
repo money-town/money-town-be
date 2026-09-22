5558697a7287217f0e8e18affc9fc61b9c624b6a
poppq03 <poppq03@naver.com>
2026-09-22 11:02:46 +0900

    fix: Feign Micrometer 빈 중복 등록 방지 (코드래빗 리뷰 반영)

--- wallet-service/src/main/java/com/moneykk/moneytown/wallet/global/config/FeignMicrometerConfig.java
+++ wallet-service/src/main/java/com/moneykk/moneytown/wallet/global/config/FeignMicrometerConfig.java
@@ -1,6 +1,5 @@
 package com.moneykk.moneytown.wallet.global.config;
 
-import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
@@ -8,11 +7,12 @@ import org.springframework.context.annotation.Bean;
 /**
  * FeignClient 자식 컨텍스트에서 MicrometerCapability 자동 생성이 타이밍 이슈로 누락되는
  * 문제 우회용(spring-cloud-openfeign#881). {@code @EnableFeignClients(defaultConfiguration = ...)}로 등록.
+ * 반환 타입을 구체 타입으로 선언해 Spring 자체 자동구성과의 중복 등록을 방지한다
  */
 public class FeignMicrometerConfig {
 
     @Bean
-    public Capability micrometerCapability(MeterRegistry meterRegistry) {
+    public MicrometerCapability micrometerCapability(MeterRegistry meterRegistry) {
         return new MicrometerCapability(meterRegistry);
     }
 }
